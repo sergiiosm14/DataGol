48ea574171fc756ef261c40088b8e60dfcc6c362
Sergio Suárez Murillo <suarezsergio426@gmail.com>
2026-05-27 11:51:57 +0200

    Mejoras Frontend

--- frontend/src/app/layout/sidebar/sidebar.ts
+++ frontend/src/app/layout/sidebar/sidebar.ts
@@ -3,10 +3,18 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  sidebarOpen = false;
 
+  openSidebar() {
+    this.sidebarOpen = true;
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
+  }
 }
